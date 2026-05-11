153f828efb64d4b8bd0f2cdef8b9f54fa98b0ef8
Igor Trindade <trindata.eng@gmail.com>
2026-05-11 20:05:35 +0000

    staging layer completa (9 modelos, sources, testes e docs)

--- models/staging/olist/stg_olist__order_items.sql
+++ models/staging/olist/stg_olist__order_items.sql
@@ -1,24 +1,27 @@
 with source as (
 
-    select * from {{ source('olsit_raw', 'order_items')}}
+    select * from {{ source('olist_raw', 'order_items') }}
 
 ),
 
 renamed as (
 
     select
-    -- ids
-    order_id,
-    order_item_id,
-    product_id,
-    seller_id,
+        -- ids (chave primária composta: order_id + order_item_id)
+        order_id,
+        order_item_id,
+        product_id,
+        seller_id,
 
-    -- datas
-    cast(shipping_limit_date as date)
+        -- datas
+        cast(shipping_limit_date as timestamp) as shipping_limit_at,
 
-    price,
-    freight_value,
+        -- valores monetários
+        cast(price         as numeric) as item_price,
+        cast(freight_value as numeric) as item_freight_value
+
+    from source
 
 )
 
-select * from rename
+select * from renamed
